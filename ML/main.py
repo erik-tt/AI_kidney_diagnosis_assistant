08538bf3005fe9b63786a6823371859bdbb50261
magge2304 <magnushtp@gmail.com>
2025-01-21 13:15:56 +0100

    Single instance of device

--- ML/main.py
+++ ML/main.py
@@ -3,26 +3,30 @@ from monai.data import DataLoader
 from train_loops.train import train_loop
 from config.model_selector import model_selector
 from utils.create_dataset import create_dataset
+import torch
 
 def main(params):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     train_dataset, test_dataset = create_dataset(params.data, params.transforms)
 
     train_dataloader = DataLoader(train_dataset, batch_size=params.batch_size, shuffle=True, num_workers=params.num_workers)
     val_dataloader = DataLoader(test_dataset, batch_size=params.batch_size, shuffle=False, num_workers=params.num_workers) 
 
-    model = model_selector(params.model)
+    model = model_selector(params.model, device)
 
     train_loop(
         model=model,
         train_dataloader=train_dataloader,
-        val_dataloader=val_dataloader
+        val_dataloader=val_dataloader,
+        device=device
         )
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
 
-    parser.add_argument("--data", nargs='+', default=["drsprg/post"], help="List of data directories")
+    parser.add_argument("--data", nargs='+', default=["drsprg/post", "drsbru/post"], help="List of data directories")
     parser.add_argument("--transforms")
     parser.add_argument("--model", default="UNet")
     parser.add_argument("--batch_size", default=4)
