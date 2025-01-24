df8a946d98f85a0103605a82ef28071397217591
magge2304 <magnushtp@gmail.com>
2025-01-24 13:18:57 +0100

    #19 Remove code used for testing

--- ML/classification/feature_extraction/radiomic_features.py
+++ ML/classification/feature_extraction/radiomic_features.py
@@ -2,9 +2,6 @@ from radiomics.featureextractor import RadiomicsFeatureExtractor
 import numpy as np
 import SimpleITK as sitk
 
-img = "../../../data/BAZA dynamicrenal/drsbru/DATA_DICOM/drsbru_004/drsbru_004_POST.dcm"
-mask = "../../../data/segmentation_dataset/drsbru/post/drsbru_004_POST/drsbru_004_POST_label.nii.gz"
-
 def extract_radiomic_features(image_series, mask):
     image = sitk.ReadImage(image_series)  
     mask = sitk.ReadImage(mask)  
@@ -34,5 +31,3 @@ def extract_radiomic_features(image_series, mask):
             radiomic_features[region][t] = results
 
     return radiomic_features
-
-extract_radiomic_features(img, mask)
