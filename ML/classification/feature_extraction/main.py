9191175d74576beabf0a8d0f6b9ca016e8a3f9b6
magge2304 <magnushtp@gmail.com>
2025-02-03 12:52:29 +0100

    #19 Extract radiomic features

--- ML/classification/feature_extraction/main.py
+++
@@ -1,7 +0,0 @@
-from ML.segmentation.utils.file_reader import FileReader
-
-file_reader = FileReader("../../../data")
-
-drsprg_post = file_reader.get_image_data("drsprg/post", ["post", "label"])
-
-
