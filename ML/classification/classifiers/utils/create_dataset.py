b1b522a15a858f2db032737b06f5f59ce5caeaa2
Magnus Lovhoiden <malovhoi@stud.ntnu.no>
2025-02-24 13:27:00 +0100

    Use sample dataset

--- ML/classification/classifiers/utils/create_dataset.py
+++ ML/classification/classifiers/utils/create_dataset.py
@@ -2,7 +2,8 @@ from typing import List
 from sklearn.model_selection import train_test_split
 from config.transforms_selector import transforms_selector
 from utils.file_reader import FileReader
-from monai.data import CacheDataset
+from dataset.SampleDataset import SampleDataset2
+from monai.data import CacheDataset, Dataset
 
 def create_dataset(transforms_name: str,
                    data_type: str,   
@@ -19,12 +20,17 @@ def create_dataset(transforms_name: str,
         random_state=random_state,     
         shuffle=shuffle 
     )
-    
     train_transforms, val_transforms = transforms_selector(transforms_name)
+
+    #train data and test data determinstic
     
-    # Check if cache fails
-    train_dataset = CacheDataset(train_data, train_transforms)
-    test_dataset = CacheDataset(test_data, val_transforms)
+    if data_type == "image":
+        train_dataset = Dataset(train_data, train_transforms)
+        test_dataset = Dataset(test_data, val_transforms)
+    else:
+        train_dataset = SampleDataset2(train_data, num_frames=20, transforms=train_transforms, agg="mean")
+        test_dataset = SampleDataset2(test_data, num_frames=20, transforms=val_transforms, agg="mean")
+
 
     sample = train_dataset[0]["image"]
     print(f"Loaded image shape: {sample.shape}")
