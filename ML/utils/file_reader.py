e77d1986226658ce672ab746d77a94ffa1622402
Magnus Lovhoiden <malovhoi@stud.ntnu.no>
2025-02-25 11:15:09 +0100

    New file reader

--- ML/utils/file_reader.py
+++ ML/utils/file_reader.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from typing import List, Optional
+import os
 
-METADATA_PATH = "../../data/metadata.csv"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get current script directory
+METADATA_PATH = os.path.join(BASE_DIR, "../../data/metadata.csv")  # Convert to absolute path
 
 def get_segmentation_data(datasets: Optional[List[str]]=None, suffixes: Optional[List[str]]=None):  
     metadata = pd.read_csv(METADATA_PATH, usecols=["ImagePath", "SegLabelPath", "Suffix", "Database"])
@@ -17,3 +19,23 @@ def get_segmentation_data(datasets: Optional[List[str]]=None, suffixes: Optional
     metadata_dict = metadata.to_dict(orient="records")
 
     return metadata_dict
+
+def get_classification_data(datasets: Optional[List[str]]=None, suffixes: Optional[List[str]]=None):  
+    metadata = pd.read_csv(METADATA_PATH, usecols=["TimeSeriesPath", "CKD", "Suffix", "Database"])
+    
+    if datasets is not None:
+        metadata = metadata[metadata["Database"].isin(datasets)]
+    
+    if suffixes is not None:
+        metadata = metadata[metadata["Suffix"].isin(suffixes)]
+
+
+    metadata = metadata[["TimeSeriesPath", "CKD"]]
+    metadata = metadata[["TimeSeriesPath", "CKD"]].dropna(subset=["CKD"])
+    metadata["CKD"] = metadata["CKD"].astype(int)
+
+    metadata.rename(columns={"TimeSeriesPath": "image", "CKD": "label"}, inplace=True)
+    metadata_dict = metadata.to_dict(orient="records")
+
+    return metadata_dict
+
