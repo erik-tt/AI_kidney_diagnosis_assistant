d7a55151385c44117b2c16fc8090916b97f90fc5
Magnus Lovhoiden <malovhoi@stud.ntnu.no>
2025-05-06 16:38:28 +0200

    Remove scale

--- ML/classification/classifiers/config/transforms_selector.py
+++ ML/classification/classifiers/config/transforms_selector.py
@@ -11,7 +11,7 @@ from monai.transforms import (
     Lambdad,
     ScaleIntensityd,
     SpatialPadd,
-    RandAffined
+    RandAffined,
 )
 
 
@@ -29,8 +29,7 @@ def transforms_selector(transforms_name :str):
     transforms = []
     if transforms_name == "config_1":
         transforms = [
-            ScaleIntensityd(keys="image", minv=0.0, maxv=1.0),
-            NormalizeIntensityd(keys=["image"], channel_wise=True, nonzero=True),  # Per-channel normalization
+            NormalizeIntensityd(keys=["image"], channel_wise=True, nonzero=True),
 
             # FOR CUDA
             Lambdad(keys=["image"], func=lambda x: x.permute(0,3,2,1)), # SJEKKE AT DET ER RIKTIG,
@@ -39,29 +38,13 @@ def transforms_selector(transforms_name :str):
             #Lambdad(keys=["image"], func=lambda x: x.permute(0,2,1,3)),
             
             SpatialPadd(keys=["image"], spatial_size=(180, -1, -1)), 
-            RandFlipd(keys="image", spatial_axis=1, prob=0.5),
-            #RandCoarseDropoutd( #decent
-            #    keys=["image"], 
-            #    holes=5,  # Number of cutout regions
-            #    spatial_size=(10, 10, 10),  # Size of each cutout block (3D)
-            #    dropout_holes=True,  # If True, replaces with zeros
-            #    fill_value=0,  # Replace cutout regions with zero intensity
-            #    prob=0.5  # Apply with 50% probability
-            #),
-            #RandGaussianNoised(keys=["image"], mean=0.0, std=0.02, prob=0.2), #Jait
-            #RandAdjustContrastd(keys=["image"], gamma=(0.9, 1.1), prob=0.5), #JAIT
-            #RandAffined( ## DEECENT
-            #    keys=["image"], 
-            #    rotate_range=(0.1, 0.1, 0.1),  # Small rotation in 3D
-            #    translate_range=(5, 5, 5),  # Small shift
-            #    scale_range=(0.1, 0.1, 0.1),  # Slight zoom in/out
-            #    prob=0.5
-            #),
+            RandFlipd(keys="image", spatial_axis=2, prob=0.5),
         ]
 
     if transforms_name == "pretrained":
         transforms = [ 
             RepeatChanneld(keys=["image"], repeats=3),
+            NormalizeIntensityd(keys=["image"], channel_wise=True, nonzero=True),
             
             # FOR CUDA
             Lambdad(keys=["image"], func=lambda x: x.permute(0,3,2,1)), # SJEKKE AT DET ER RIKTIG,
@@ -70,9 +53,7 @@ def transforms_selector(transforms_name :str):
             #Lambdad(keys=["image"], func=lambda x: x.permute(0,2,1,3)),
 
             Resized(keys=["image"], spatial_size=[-1, 224, 224]), 
-            ScaleIntensityd(keys="image", minv=0.0, maxv=1.0),
-            NormalizeIntensityd(keys="image"),
-            RandFlipd(keys="image", spatial_axis=1, prob=0.5),
+            RandFlipd(keys="image", spatial_axis=2, prob=0.5),
         ]
 
     train_transforms = PRE_TRANSFORMS + transforms + POST_TRANSFORMS
