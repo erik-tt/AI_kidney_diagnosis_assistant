7f6025a1cc02d039171a2ae62f00a9308805ab1f
magge2304 <magnushtp@gmail.com>
2025-05-13 10:40:51 +0200

    Correct order transforms

--- ML/classification/classifiers/config/transforms_selector.py
+++ ML/classification/classifiers/config/transforms_selector.py
@@ -29,28 +29,31 @@ def transforms_selector(transforms_name :str):
     transforms = []
     if transforms_name == "config_1":
         transforms = [
+            # FOR NOT CUDA
+
+            Lambdad(keys=["image"], func=lambda x: x.permute(0,2,1,3)),
+            
+            SpatialPadd(keys=["image"], spatial_size=(180, -1, -1), method="end"),
+            
             NormalizeIntensityd(keys=["image"], channel_wise=True, nonzero=True),
 
             # FOR CUDA
             #Lambdad(keys=["image"], func=lambda x: x.permute(0,3,2,1)), # SJEKKE AT DET ER RIKTIG,
             
-            # FOR NOT CUDA
-            Lambdad(keys=["image"], func=lambda x: x.permute(0,2,1,3)),
-            
-            SpatialPadd(keys=["image"], spatial_size=(180, -1, -1), method="end"),
             RandFlipd(keys="image", spatial_axis=2, prob=0.5),
         ]
 
     if transforms_name == "pretrained":
         transforms = [ 
-            RepeatChanneld(keys=["image"], repeats=3),
-            NormalizeIntensityd(keys=["image"], channel_wise=True, nonzero=True),
+            Lambdad(keys=["image"], func=lambda x: x.permute(0,2,1,3)),
             
+            NormalizeIntensityd(keys=["image"], channel_wise=True, nonzero=True),
+
+            RepeatChanneld(keys=["image"], repeats=3),
             # FOR CUDA
             #Lambdad(keys=["image"], func=lambda x: x.permute(0,3,2,1)), # SJEKKE AT DET ER RIKTIG,
             
             # FOR NOT CUDA
-            Lambdad(keys=["image"], func=lambda x: x.permute(0,2,1,3)),
 
             Resized(keys=["image"], spatial_size=[-1, 224, 224]), 
             RandFlipd(keys="image", spatial_axis=2, prob=0.5),
